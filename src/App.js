cd53542414abf40d2ca56e70828ae6c3298838d1
Simon Tharby <simontharby@gmail.com>
2022-02-02 20:31:41 +0000

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import { useState } from 'react';
 
 function App() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState('<your name>');
   const handleChange = (name) => {
     setName(name);
   };
 
   return (
-    <div className="App">
-      <ChildComponent onChange={handleChange} />
-      <h1>{name}</h1>     
+    <div className="container app">
+      <div className='row'>
+        <h3>App component [parent]</h3>
+      </div>
+      <div className='row'>
+        <h1>My name is {name}</h1>
+      </div>
+      <form className='row g-3'>
+        
+          <NameInput onChange={handleChange} />
+           
+          <NameInput onChange={handleChange} />
+
+      </form>
     </div>
   );
 }
 
-const ChildComponent = ({ onChange, name }) => {
+const NameInput = ({ onChange, name }) => {
   return (
-    <input
-      value={name}
-      onChange={e => onChange(e.target.value)}
-    />
+    <div className='col-lg-6 card input'>
+      <h3>Input component [child of App]</h3>
+      <label htmlFor='name' className='form-label'>Name</label>
+      <input
+        className='form-control'
+        placeholder='Name'
+        value={name}
+        onChange={e => onChange(e.target.value)}
+      />
+    </div>  
   )
 };
 
