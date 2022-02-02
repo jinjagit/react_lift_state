164e4fb8df6084cc40d5256a14ec26191929db17
Simon Tharby <simontharby@gmail.com>
2022-02-02 19:48:47 +0000

    lift state to parent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const handleChange = (name) => {
+    setName(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChildComponent onChange={handleChange} />
+      <h1>{name}</h1>     
     </div>
   );
 }
+
+const ChildComponent = ({ onChange, name }) => {
+  return (
+    <input
+      value={name}
+      onChange={e => onChange(e.target.value)}
+    />
+  )
+};
 
 export default App;
